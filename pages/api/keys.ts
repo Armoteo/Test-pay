8e0ce2b6eddda0fd77380166a928c827d94e9284
Viktor Korotkiy <55664118+Armoteo@users.noreply.github.com>
2023-04-12 18:48:05 +0300

    remove

--- pages/api/keys.ts
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    req.status(200).json({
-      publishabKey: process.env.NEXT_PUBLIC_STRIPE_KEY,
-    });
-  } else {
-    res.status(405).end('Method not allowed');
-  }
-}
